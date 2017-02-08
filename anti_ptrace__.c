17579564733ad8cb5312deb4f4a60fc9cb909f49
Seokha Ko <sh.ko@lge.com>
2017-02-08 06:11:44 -0500

    anti ptrace.

---
+++ anti_ptrace__.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main()
+{
+    pid_t child;
+    child = fork();
+
+    if(child == 0)
+    {
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        execl("/bin/ls", "ls", NULL);
+    }
+    else
+    {
+        wait(NULL);
+    }
+
+    printf("exit, pid: %d \n", child);
+
+    return 0;
+}
